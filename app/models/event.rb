50e5c8a2887b6292ebab1005aaf03bef5586b8d5
Bryan Yap <bryan.yap@alliants.com>
2015-05-03 01:01:57 +0100

    add default profile image for event hosts without profile images

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,7 +18,7 @@ class Event < ActiveRecord::Base
   end
 
   def host_profile_image_url
-    user ? user.profile_image_url : 'http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y'
+    (user && user.profile_image_url) ? user.profile_image_url : 'http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y'
   end
 
   def date_not_in_the_past
