c415f90aec0fccc439b55eef6b726e368f23a3df
Bryan Yap <bryan.yap@alliants.com>
2015-05-05 10:02:26 +0100

    fixed bug with events accepting the current date

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,10 +9,10 @@ class Event < ActiveRecord::Base
   validates :summary, presence: true
   validates :date, presence: true
 
-  validate :date_not_in_the_past
+  validate :date_not_today_or_in_the_past
 
   scope :upcoming, -> { where(['date > ?', Date.today]).order('events.date ASC') }
-  scope :archived, -> { where(['date < ?', Date.today]).order('events.date ASC') }
+  scope :archived, -> { where(['date <= ?', Date.today]).order('events.date ASC') }
 
   def host_name
     user ? user.name : 'Guest host'
@@ -22,8 +22,8 @@ class Event < ActiveRecord::Base
     (user && user.profile_image_url) ? user.profile_image_url : 'http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y'
   end
 
-  def date_not_in_the_past
-    if self.date && self.date < Date.today
+  def date_not_today_or_in_the_past
+    if self.date && self.date <= Date.today
       self.errors.add(:date, 'cannot be in the past')
     end
   end
